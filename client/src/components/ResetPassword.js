81f61d0892f20b65cd6fbf13b5962c09e0337ff8
kevd82 <barbaricsplendor@gmail.com>
2023-07-13 21:48:02 -0700

    further work on reset password

---
+++ client/src/components/ResetPassword.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import {Link, useNavigate, useParams} from "react-router-dom";
+
+
+const ResetPassword = (props)=>{
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate();
+    const {id} = useParams();
+
+    useEffect(()=>{
+        axios.get(`http://localhost:8000/api/user/${id}`)
+        .then((res)=>{
+            console.log(res);
+            console.log(res.data);
+            setPassword(res.data.password);
+        })
+        .catch((err)=>console.log(err))
+    }, [id])
+}
+
+const submitResetHandler=((e)=>{
+    e.preventDefault();
+})
