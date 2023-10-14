c603c9601204b15942501c3ec2aacdcdbf2a3cd8
Your Name <you@example.com>
2023-10-14 14:07:43 -0700

    started on ResetPassword.js

--- client/src/components/ResetPassword.js
+++ client/src/components/ResetPassword.js
@@ -4,21 +4,54 @@ import {useNavigate, useParams} from "react-router-dom";
 
 
 const ResetPassword = (props)=>{
-    const [password, setPassword] = useState("")
+    const [passwprd, setPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const {confirmNewPassword, setNewConfirmPassword} = useState("");
     const navigate = useNavigate();
     const {id} = useParams();
+    const [errors, setErrors]= useState("")
 
     useEffect(()=>{
         axios.get(`http://localhost:8000/api/user/${id}`)
         .then((res)=>{
             console.log(res);
             console.log(res.data);
-            setPassword(res.data.password);
+            setPassword(res.data.password)
         })
         .catch((err)=>{
             console.log(err);
 
-        }, );
+        }, [id]);
+
+
+        const submitHandler=((e)=>{
+            e.preventDefault();
+            newPassword===confirmNewPassword?
+            axios.put(`http://localhost:8000/api/user/${id}`,
+            {
+                password: newPassword
+            })
+            .then((res)=>{
+                console.log(res);
+                console.log(res.data);
+                setErrors("");
+                navigate("/");
+
+            })
+            .catch((err)=>{
+                console.log("err.response:", err.response);
+                console.log("err.response.data", err.response.data);
+                console.log("err.response.data.errors", err.response.data.errors);
+                setErrors(err.response.data.errors);
+    
+            })
+            :navigate("/resetPassword/:id")
+
+
+
+
+
+        })
 
 
 
@@ -30,7 +63,7 @@ const ResetPassword = (props)=>{
 
 return (
     <div>
-        <h1>reset page</h1>
+        <h1>Password Reset</h1>
     </div>
 )
 }
